cb32fcb8c3c400edbfcf4b1ea4e6d3c1d5e8a82c
Raaghav321 <rawasthi2005@gmail.com>
2019-10-15 08:36:04 -0500

    Archimedes 1

--- Week 3.py
+++ Week 3.py
@@ -24,6 +24,28 @@ for sides in range (8,100,8):
 # experiment with the loop above alongside the actual value of Pi.How many
 # sides does it take to make the two close?
 
+#in between two and tree
+
+
+#Accumulators
+
+acc = 0
+for x in range(1,6):
+    acc = acc + x
+
+print(acc)
+
+#compute the sum of the first 100 even numbers
+#compute the sum of the first 50 odd numbers
+#compute the average of the first 100 odd numbers
+#   N is a parameter
+#write a function called factorial that computes the product of the first N
+#Each number is the Fibonacchi sequence  id the sum of the previous 2 numbers
+#   The first numbers in the sequence are 1 and 1. Compute the 10th
+#   Fibonacci Sequence
+#Write a function to compute the Nth Fibonacci Sequence, Where N is the parameter
+#You may assume that N will be greater than or equal to 3
+
 
 
 
