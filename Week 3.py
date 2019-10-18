7cbd2bd7465628b670089ebf7d3638f272b06190
Raaghav321 <rawasthi2005@gmail.com>
2019-10-18 09:02:53 -0500

    Friaday 5

--- Week 3.py
+++ Week 3.py
@@ -26,7 +26,6 @@ for sides in range (8,100,8):
 
 #in between two and tree
 
-
 #Accumulators
 
 acc = 0
@@ -44,8 +43,84 @@ print(acc)
 #   The first numbers in the sequence are 1 and 1. Compute the 10th
 #   Fibonacci Sequence
 #Write a function to compute the Nth Fibonacci Sequence, Where N is the parameter
+
 #You may assume that N will be greater than or equal to 3
 #AVERAGE=83.33
+
+# A Monte Carlo Simulation
+
+import random
+
+print(random.random())
+
+# Boolean expressions
+#> greater than
+# >=grater than or equal to
+# < less than
+# <= less than or equal to
+# == the same as [equal to]           # comparing 2 things use 2
+#! Not equal to
+
+dogWeight = 25
+print(dogWeight != 25)
+catWeight = 15
+
+#coumpound boolean operaters
+#and
+#or
+#not
+
+print(dogWeight < 30 or catWeight < 20)  #False= not, Or lighten load
+
+
+#Desicion Making -- Selection Statements
+a=5
+b=10
+c=75
+
+if a > b :
+    c=45
+
+print(c)
+
+if a > b :
+    c=45
+    if b>c:
+
+else:
+  a=25
+else :
+    c = 1050
+    if b==a:
+        c=25
+
+print(a,b,c)
+
+
+d = 55
+e = 72
+f = 44
+ans = 0
+
+
+if d>e:
+    ans=12
+
+if d==e:
+    ans=50
+else:
+    if f>d * e:
+        ans =100
+    else:
+        ans=75
+print(ans)
+
+
+
+
+
+print(c)
+
 
 
 
